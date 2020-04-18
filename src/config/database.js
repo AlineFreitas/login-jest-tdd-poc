27f1c96ec87f41accb63a0a5c13aa7a1b5659bdf
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-18 11:50:38 -0300

    Customized src/config/database.js file to work with sqlite

--- src/config/database.js
+++ src/config/database.js
@@ -1,26 +1,41 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+module.exports = {
+  host: '127.0.0.1',
+  username: 'root',
+  password: null,
+  database: 'login-jest-tdd',
+  dialect: 'sqlite',
+  operatorsAliases: '0',
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
   }
-}
+};
+
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_development",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   }
+// }
