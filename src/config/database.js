a6876ae59bc4eb14c19f7d390513d95e2b49df98
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-18 16:14:43 -0300

    Updates .env.sample file and src/config/database.js to use .env variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,16 @@
+const path = require('path');
+
+require('dotenv').config({
+  path: process.env.NODE_ENV = "test" ? ".env.test" : ".env"
+});
+
 module.exports = {
-  host: '127.0.0.1',
-  username: 'root',
-  password: null,
-  database: 'login-jest-tdd',
-  dialect: 'sqlite',
+  host: process.env.DB_HOST_ADDRESS,
+  username: process.env.DB_USER_NAME,
+  password: process.env.DB_USER_PASSWORD,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
+  storage: path.join(__dirname, '..', '..', '__tests__/', 'test.sqlite'),
   operatorsAliases: '0',
   logging: false,
   define: {
