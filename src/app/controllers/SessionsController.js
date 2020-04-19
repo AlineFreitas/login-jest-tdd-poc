6698e6dbfd49ac029d170dcd04684e2024af64db
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-19 19:57:28 -0300

    Modifies user controller to return 404 if no user is found

--- src/app/controllers/SessionsController.js
+++ src/app/controllers/SessionsController.js
@@ -5,6 +5,10 @@ class SessionsController {
     const { email, password } = request.body;
     const user = await User.findOne({ where: { email } });
 
+    if (!user){
+      return response.status(404).send();
+    }
+
     if(! (await user.confirmPassword(password)) ) {
       return response.status(401).send();
     }
