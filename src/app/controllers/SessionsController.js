5e5e460060ed90ab567b371b8cc1ec393a85e400
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-19 12:27:08 -0300

    Modifies Sessions Controller to return HTTP Status 401 if user's password don't match

--- src/app/controllers/SessionsController.js
+++ src/app/controllers/SessionsController.js
@@ -1,5 +1,13 @@
+const { User } = require('../models');
+
 class SessionsController {
   async signin(request, response) {
+    const { email, password } = request.body;
+    const user = await User.findOne({ where: { email } });
+
+    if(! (await user.confirmPassword(password)) ) {
+      return response.status(401).send();
+    }
     return response.status(200).send();
   }
 }
