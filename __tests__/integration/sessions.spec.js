0430fa5ef7d75e6ae827e2b12c8d60cc6ba6d1c8
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-19 23:42:31 -0300

    Adds new describe group on sessions integration test to separate Sign in from

--- __tests__/integration/sessions.spec.js
+++ __tests__/integration/sessions.spec.js
@@ -11,52 +11,54 @@ describe('Authorization', () => {
     await truncate();
     user = await factory.create('User');
   });
-  describe('On success', () =>{
+  describ('Sign In', () => {
+    describe('On success', () =>{
 
-    it('authenticates with valid credentials', async() => {
-      const response = await request(app)
-      .post('/signin')
-      .send({
-        email: user.email,
-        password: user.password
-      });
-  
-      expect(response.status).toBe(200);
-    });
-
-    it('returns jwt token when sucessfully authenticated', async() => {
-      const response = await request(app)
-      .post('/signin')
-      .send({
-        email: user.email,
-        password: user.password
-      });
-
-      expect(response.body).toHaveProperty('token');
-    });
-  });
-
-  describe('', () => {
-    it('doesn\'t authenticate with invalid credentials', async() => {
-      const response = await request(app)
+      it('authenticates with valid credentials', async() => {
+        const response = await request(app)
         .post('/signin')
         .send({
           email: user.email,
-          password: '123123'
+          password: user.password
         });
+    
+        expect(response.status).toBe(200);
+      });
   
-        expect(response.status).toBe(401);
-    });
-  
-    it('returns 404 if no user is found with given credentials', async() => {
-      const response = await request(app)
+      it('returns jwt token when sucessfully authenticated', async() => {
+        const response = await request(app)
         .post('/signin')
         .send({
-          email: 'non-existent@email.com',
-          password: 'password'
+          email: user.email,
+          password: user.password
         });
   
-        expect(response.status).toBe(404);
+        expect(response.body).toHaveProperty('token');
+      });
+    });
+  
+    describe('', () => {
+      it('doesn\'t authenticate with invalid credentials', async() => {
+        const response = await request(app)
+          .post('/signin')
+          .send({
+            email: user.email,
+            password: '123123'
+          });
+    
+          expect(response.status).toBe(401);
+      });
+    
+      it('returns 404 if no user is found with given credentials', async() => {
+        const response = await request(app)
+          .post('/signin')
+          .send({
+            email: 'non-existent@email.com',
+            password: 'password'
+          });
+    
+          expect(response.status).toBe(404);
+      });
     });
   });
 });
