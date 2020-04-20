0136bd5a7767b6d388d2f19bcd31fcfdefe61468
Aline Freitas <aline.do.freitas@gmail.com>
2020-04-19 22:00:59 -0300

    On success controller returns generated token and user object

--- src/app/controllers/SessionsController.js
+++ src/app/controllers/SessionsController.js
@@ -12,7 +12,11 @@ class SessionsController {
     if(! (await user.confirmPassword(password)) ) {
       return response.status(401).send();
     }
-    return response.status(200).send();
+
+    return response.status(200).json({
+      user,
+      token: user.generateToken()
+    });
   }
 }
 
